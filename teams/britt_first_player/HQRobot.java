f1cba6956ad32b064aef21c237ae10f55a673993
Britt Cyr <cyrbritt@mit.edu>
2014-01-07 17:49:39 -0500

    improving pastr code on britt_first_player

--- teams/britt_first_player/HQRobot.java
+++ teams/britt_first_player/HQRobot.java
@@ -2,7 +2,10 @@ package britt_first_player;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.Robot;
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
 
 public class HQRobot extends BaseRobot {
 
@@ -13,6 +16,15 @@ public class HQRobot extends BaseRobot {
 
     public void run() {
         try {
+            Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class, 10, rc.getTeam()
+                    .opponent());
+            if (nearbyEnemies.length > 0) {
+                RobotInfo robotInfo = rc.senseRobotInfo(nearbyEnemies[0]);
+                if (robotInfo.type == RobotType.SOLDIER) {
+                    rc.attackSquare(robotInfo.location);
+                    return;
+                }
+            }
             // Check if a robot is spawnable and spawn one if it is
             if (rc.isActive() && rc.senseRobotCount() < 25) {
                 Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
@@ -20,10 +32,18 @@ public class HQRobot extends BaseRobot {
                     rc.spawn(toEnemy);
                 }
             }
+
+            else {
+                if (nearbyEnemies.length > 0) {
+                    RobotInfo robotInfo = rc.senseRobotInfo(nearbyEnemies[0]);
+                    if (robotInfo.type == RobotType.SOLDIER) {
+                        rc.attackSquare(robotInfo.location);
+                    }
+                }
+            }
         }
         catch (Exception e) {
             System.out.println("HQ Exception");
         }
     }
-
 }
