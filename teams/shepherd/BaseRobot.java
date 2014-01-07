099e7a218719814f3de01b1f7c113a545ca68da6
Webb Horn <webbhorn@mit.edu>
2014-01-07 10:40:25 -0500

    First iteration at shepherdbot. Cowboys spread out in general direction of enemyhq and milk cows when they get to a good area.

---
+++ teams/shepherd/BaseRobot.java
@@ -0,0 +1,27 @@
+package shepherd;
+
+import java.util.Random;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public abstract class BaseRobot {
+    public static RobotController rc;
+    public final static Random    rand = new Random();
+
+    public BaseRobot(RobotController myRC) throws GameActionException {
+        rc = myRC;
+    }
+
+    public void loop() {
+        if (rc.isActive()) {
+            run();
+        }
+        rc.yield();
+    }
+
+    public void run() {
+        // pass
+    }
+
+}
