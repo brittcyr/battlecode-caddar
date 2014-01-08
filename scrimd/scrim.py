bd8b5c6b80db82cef4663cf1b82ac515de8c1afc
Webb Horn <webbhorn@mit.edu>
2014-01-08 18:50:13 -0500

    Update proto files, scrim stoes java stdout and stderr in console.log file.

--- scrimd/scrim.py
+++ scrimd/scrim.py
@@ -4,6 +4,7 @@ import datetime
 import os
 from random import shuffle
 import shutil
+import subprocess
 
 bots_file = "bots.txt"
 maps_file = "maps.txt"
@@ -15,16 +16,26 @@ with open(maps_file) as f:
   maps = [m.strip()[:-4] for m in f.readlines()]
 
 def preparegame(a, b, m, gamedir):
-  pass
+  with open(os.path.join(gamedir, "bc.conf"), "a") as f:
+    f.write("\n\n## BEGIN SCRIMD ##\n")
+    f.write("bc.game.maps=%s\n" % m)
+    f.write("bc.game.team-a=%s\n" % a)
+    f.write("bc.game.team-a=%s\n" % b)
 
 def playmatch(a, b, matchdir):
+  print "%s vs %s... " % (a, b)
   for m in maps:
     gamedir = os.path.join(matchdir, m)
     os.mkdir(gamedir)
     shutil.copy("build.xml.proto", os.path.join(gamedir, "build.xml"))
     shutil.copy("bc.conf.proto", os.path.join(gamedir, "bc.conf"))
     preparegame(a, b, m, gamedir)
-    # play game
+    cwd = os.getcwd()
+    with open(os.path.join(gamedir, "console.log"), "w") as f:
+      os.chdir(gamedir)
+      subprocess.call(["ant", "file"], stdout=f, stderr=f)
+    os.chdir(cwd)
+  print "done\n"
 
 matchups = []
 for a in range(len(bots)):
