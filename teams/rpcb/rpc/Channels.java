0a4a900ae353d8f58330a443c00f5eadc3ed74a6
Webb Horn <webbhorn@mit.edu>
2014-01-13 15:12:25 -0500

    Some untested clan RPC code.

---
+++ teams/rpcb/rpc/Channels.java
@@ -0,0 +1,48 @@
+package rpcb.rpc;
+
+/*
+ * Treat radio channels like a big address space. Each channel can store an int (32 bits) of data.
+ * We have 2^16 = 0x10000 = 65536 channels. Then each channel addresses a single 32-bit word.
+ * 
+ *      +-------------------+
+ *      |  (32 bits)        |
+ *      +-------------------+  <- Channel 65536
+ *      |                   |
+ *      |                   |
+ *      \/\/\/\/\/\/\/\/\/\/\
+ *      
+ *      
+ *      /\/\/\/\/\/\/\/\/\/\/
+ *      |                   |
+ *      +-------------------+
+ *      |                   |
+ *      |  Clan             |
+ *      |  Address Space    |
+ *      |                   |
+ *      |                   |
+ *      +-------------------+  <- CLAN_ADDR_SPACE_START
+ *      |                   |
+ *      \/\/\/\/\/\/\/\/\/\/\
+ *      
+ *      
+ *      /\/\/\/\/\/\/\/\/\/\/
+ *      |                   |
+ *      |                   |
+ *      +-------------------+
+ *      |                   |
+ *      +-------------------+  <- Channel 0x00000
+ *      
+ * This class stores common pointers into this "address space."
+ */
+
+public class Channels {
+
+    // Clan information.
+    public static final int CLAN_ADDR_SPACE_START = 60000;
+    public static final int CLAN_MEMBERSHIPS      = CLAN_ADDR_SPACE_START;
+    public static final int MAX_GAME_OBJS         = 1009;                 // 1009 prime.
+    public static final int CLAN_SIZES            = CLAN_MEMBERSHIPS + MAX_GAME_OBJS;
+    public static final int CLAN_WAYPOINTS        = CLAN_SIZES + MAX_GAME_OBJS;
+
+
+}
---
+++ teams/rpcb/rpc/Radio.java
@@ -0,0 +1,34 @@
+package rpcb.rpc;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Radio {
+
+    public static RobotController rc;
+
+    public Radio(RobotController rc) {
+        Radio.rc = rc;
+    }
+
+    /*
+     * Retrieve data from radio.
+     */
+    public static int[] getData(int channel, int sz) throws GameActionException {
+        int[] data = new int[sz];
+        for (int i = 0; i < sz; i++) {
+            data[i] = rc.readBroadcast(channel + i);
+        }
+        return data;
+    }
+
+    /*
+     * Write data to radio.
+     */
+    public static void putData(int channel, int[] data) throws GameActionException {
+        for (int i = 0; i < data.length; i++) {
+            rc.broadcast(channel + i, data[i]);
+        }
+    }
+
+}
