b53454bf0d7ccb9299f9d185ae6331e9b186b7ef
Webb Horn <webbhorn@mit.edu>
2014-01-15 16:22:46 -0500

    Simple clan demonstration.

---
+++ teams/new_navigator_clans/RobotPlayer.java
@@ -0,0 +1,44 @@
+package new_navigator_clans;
+
+import java.util.Random;
+
+import battlecode.common.RobotController;
+
+public class RobotPlayer {
+    static Random rand;
+
+    public static void run(RobotController rc) {
+        BaseRobot br = null;
+        try {
+            switch (rc.getType()) {
+                case HQ:
+                    br = new HQRobot(rc);
+                    break;
+                case SOLDIER:
+                    br = new CowboyRobot(rc);
+                    break;
+                case PASTR:
+                    br = new PastrRobot(rc);
+                    break;
+                case NOISETOWER:
+                    br = new NoiseTowerRobot(rc);
+                    break;
+                default:
+                    throw new Exception("FAILED TO INITIALIZE");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Main loop should never terminate
+        while (true) {
+            try {
+                br.loop();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
