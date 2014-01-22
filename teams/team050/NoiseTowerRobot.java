6ddda67012ca3f29b1ac53827ad9de5ba71dfe68
brittcyr <cyrbritt@mit.edu>
2014-01-22 06:14:38 -0500

    first attempt at noise tower

--- teams/team050/NoiseTowerRobot.java
+++ teams/team050/NoiseTowerRobot.java
@@ -1,12 +1,26 @@
 package team050;
 
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.Team;
 
 public class NoiseTowerRobot extends BaseRobot {
+    public final Team  me;
+    public final Team  enemy;
+    public MapLocation myPastr;
+    public MapLocation enemyPastr;
+    public MapLocation target;
+    public Direction   dir;
+    public int         dist;
 
     public NoiseTowerRobot(RobotController myRC) throws GameActionException {
         super(myRC);
+        me = rc.getTeam();
+        enemy = me.opponent();
+        dir = Direction.NORTH;
+        dist = 20;
     }
 
     protected void getUpdates() {
@@ -14,11 +28,32 @@ public class NoiseTowerRobot extends BaseRobot {
     }
 
     protected void updateInternals() {
-        // pass
+
     }
 
     public void doAction() throws GameActionException {
-        // pass
+        MapLocation[] myPastrs = rc.sensePastrLocations(me);
+        MapLocation[] enemyPastrs = rc.sensePastrLocations(enemy);
+
+        if (enemyPastrs.length > 0) {
+            // Be offensive
+            target = enemyPastrs[0];
+        }
+        if (myPastrs.length > 0) {
+            myPastr = myPastrs[0];
+            dist -= 5;
+            if (dist < 5) {
+                dir = dir.rotateRight();
+                dir = dir.rotateRight();
+                dir = dir.rotateRight();
+                dist = 15;
+            }
+            target = myPastr.add(dir, dist);
+        }
+
+        if (rc.canAttackSquare(target)) {
+            rc.attackSquare(target);
+        }
     }
 
     protected void sendUpdates() {
