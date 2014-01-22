1dc3049f4dba317858e7f525c73af63483d1dbe1
Britt Cyr <cyrbritt@mit.edu>
2014-01-22 14:46:08 -0500

    Do not attempt directions that are completely blocked

--- teams/team050/NoiseTowerRobot.java
+++ teams/team050/NoiseTowerRobot.java
@@ -1,11 +1,13 @@
 package team050;
 
+import battlecode.common.Clock;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.Robot;
 import battlecode.common.RobotController;
 import battlecode.common.Team;
+import battlecode.common.TerrainTile;
 
 public class NoiseTowerRobot extends BaseRobot {
     public final Team  me;
@@ -45,20 +47,48 @@ public class NoiseTowerRobot extends BaseRobot {
             }
         }
 
-        if (enemyPastrs.length > 0) {
-            // Be offensive
-            target = enemyPastrs[0];
-        }
         if (myPastrs.length > 0) {
+            // Pick the nearest pastr
             myPastr = myPastrs[0];
+            for (MapLocation p : myPastrs) {
+                myPastr = p.distanceSquaredTo(rc.getLocation()) < myPastr.distanceSquaredTo(rc
+                        .getLocation()) ? p : myPastr;
+            }
+
             dist -= 5;
             if (dist < 5) {
                 dir = dir.rotateRight();
                 dir = dir.rotateRight();
                 dir = dir.rotateRight();
+
+                for (int x = 0; x < 9; x++) {
+                    if (!checkDirection(myPastr, dir)) {
+                        dir = dir.rotateRight();
+                        dir = dir.rotateRight();
+                        dir = dir.rotateRight();
+                    }
+                    else {
+                        break;
+                    }
+                }
+
                 dist = 15;
             }
+
             target = myPastr.add(dir, dist);
+        }
+
+        if (enemyPastrs.length > 0) {
+            enemyPastr = enemyPastrs[0];
+            for (MapLocation p : enemyPastrs) {
+                int distToEnemy = p.distanceSquaredTo(rc.getLocation());
+                enemyPastr = distToEnemy > enemyPastr.distanceSquaredTo(rc.getLocation())
+                        && distToEnemy <= 300 ? p : enemyPastr;
+            }
+            // Be offensive
+            if (rc.canAttackSquare(enemyPastr)) {
+                target = enemyPastr;
+            }
         }
 
         if (rc.canAttackSquare(target)) {
@@ -73,6 +103,38 @@ public class NoiseTowerRobot extends BaseRobot {
     protected void doCompute() {
         // pass
         // TODO: Do spare computing for offense
+    }
+
+    public boolean checkDirection(MapLocation pastr, Direction dir) {
+        MapLocation target = pastr;
+
+        while (rc.canAttackSquare(target)) {
+            TerrainTile terrain = rc.senseTerrainTile(target);
+            if (terrain == TerrainTile.OFF_MAP) {
+                break;
+            }
+            if (terrain == TerrainTile.NORMAL || terrain == TerrainTile.ROAD) {
+                target = target.add(dir);
+            }
+            else {
+                MapLocation left = target.add(dir.rotateLeft().rotateLeft());
+                MapLocation right = target.add(dir.rotateRight().rotateRight());
+
+                TerrainTile leftTerrain = rc.senseTerrainTile(left);
+                TerrainTile rightTerrain = rc.senseTerrainTile(right);
+
+                if (leftTerrain == TerrainTile.NORMAL || leftTerrain == TerrainTile.ROAD
+                        || rightTerrain == TerrainTile.NORMAL || rightTerrain == TerrainTile.ROAD) {
+                    target = target.add(dir);
+                }
+                else {
+                    // Otherwise we are on a void (maybe off the map, need to test) and left and
+                    // right are as well
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
