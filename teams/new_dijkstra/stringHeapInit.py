45f47c621f133a148d0e8e0779a28fa192241dd5
brittcyr <cyrbritt@mit.edu>
2014-01-22 05:40:03 -0500

    save two turns by better compilation

--- teams/new_dijkstra/stringHeapInit.py
+++ teams/new_dijkstra/stringHeapInit.py
@@ -2,8 +2,19 @@
 vals = ['X' + str(x) + 'Y' for x in range(1000)]
 vals = [x + 'WWWWWWW' for x in vals]
 vals = [x[:5] for x in vals]
-print '\"' + ''.join(vals) + '\"'
+#print '\"' + ''.join(vals) + '\"'
 
+s = ""
+for a in range(1,51):
+	s = ""
+	for b in range(1,51):
+		s += ' + \"Z\" + (char)' + str(a) + ' + (char)' + str(b)
+	s = s[3:]
+	s = "data.append(" + s
+	s += ");"
+	print s
+
+		
 
 #chars = [['Z', chr(a), chr(b)] for a in range(50) for b in range(100)]
 #chars = ''.join([''.join(x) for x in chars])
