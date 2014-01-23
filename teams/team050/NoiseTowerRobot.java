79fb2eec79c6280bc5c14111c27b91117d2fc429
Britt Cyr <cyrbritt@mit.edu>
2014-01-23 14:28:07 -0500

    improving noise tower line choosing

--- teams/team050/NoiseTowerRobot.java
+++ teams/team050/NoiseTowerRobot.java
@@ -9,13 +9,15 @@ import battlecode.common.Team;
 import battlecode.common.TerrainTile;
 
 public class NoiseTowerRobot extends BaseRobot {
-    public final Team  me;
-    public final Team  enemy;
-    public MapLocation myPastr;
-    public MapLocation enemyPastr;
-    public MapLocation target;
-    public Direction   dir;
-    public int         dist;
+    public final Team    me;
+    public final Team    enemy;
+    public MapLocation   myPastr;
+    public MapLocation   enemyPastr;
+    public MapLocation   target;
+    public MapLocation[] myPastrs;
+    public MapLocation[] enemyPastrs;
+    public Direction     dir;
+    public int           dist;
 
     public NoiseTowerRobot(RobotController myRC) throws GameActionException {
         super(myRC);
@@ -30,49 +32,22 @@ public class NoiseTowerRobot extends BaseRobot {
     }
 
     protected void updateInternals() {
-        // TODO: Compute dijkstra on a really coarse map, 4x4 on region around noise tower
-        // But this weighting does not care about roads
-    }
+        myPastrs = rc.sensePastrLocations(me);
+        enemyPastrs = rc.sensePastrLocations(enemy);
 
-    public void doAction() throws GameActionException {
-        MapLocation[] myPastrs = rc.sensePastrLocations(me);
-        MapLocation[] enemyPastrs = rc.sensePastrLocations(enemy);
+        myPastr = null;
+        enemyPastr = null;
 
+        // Pick the nearest pastr
         if (myPastrs.length > 0) {
-            // Pick the nearest pastr
             myPastr = myPastrs[0];
             for (MapLocation p : myPastrs) {
                 myPastr = p.distanceSquaredTo(rc.getLocation()) < myPastr.distanceSquaredTo(rc
                         .getLocation()) ? p : myPastr;
             }
-
-            dist -= 4;
-            if (dist < 4) {
-                dir = dir.rotateRight();
-                dir = dir.rotateRight();
-                dir = dir.rotateRight();
-
-                for (int x = 0; x < 9; x++) {
-                    if (!checkDirection(myPastr, dir)) {
-                        dir = dir.rotateRight();
-                        dir = dir.rotateRight();
-                        dir = dir.rotateRight();
-                    }
-                    else {
-                        break;
-                    }
-                }
-
-                dist = 15;
-                // This tries to handle the off the map situation
-                while (!rc.canAttackSquare(myPastr.add(dir, dist)) && dist > 0) {
-                    dist -= 1;
-                }
-            }
-
-            target = myPastr.add(dir, dist);
         }
 
+        // Pick the farthest in range
         if (enemyPastrs.length > 0) {
             enemyPastr = enemyPastrs[0];
             for (MapLocation p : enemyPastrs) {
@@ -80,7 +55,47 @@ public class NoiseTowerRobot extends BaseRobot {
                 enemyPastr = distToEnemy > enemyPastr.distanceSquaredTo(rc.getLocation())
                         && distToEnemy <= 300 ? p : enemyPastr;
             }
-            // Be offensive
+        }
+
+        target = null;
+
+        // TODO: Compute dijkstra on a really coarse map, 4x4 on region around noise tower
+        // But this weighting does not care about roads
+    }
+
+    public void doAction() throws GameActionException {
+
+        // Leave this code here since we only want it to run whenever the NT is active
+        if (myPastr != null) {
+            dist -= 3;
+            if (dist < 4) {
+                dir = dir.rotateRight();
+                dir = dir.rotateRight();
+                dir = dir.rotateRight();
+
+                for (int x = 0; x < 9; x++) {
+                    int dirDist = checkDirection(myPastr, dir);
+                    if (dirDist < 10) {
+                        dir = dir.rotateRight();
+                        dir = dir.rotateRight();
+                        dir = dir.rotateRight();
+                    }
+                    else {
+                        dist = dirDist;
+                        break;
+                    }
+                }
+
+                // This tries to handle the off the map situation
+                while (!rc.canAttackSquare(myPastr.add(dir, dist)) && dist > 0) {
+                    dist -= 1;
+                }
+            }
+            target = myPastr.add(dir, dist);
+        }
+
+        if (enemyPastr != null) {
+            // Be offensive only if they are in range
             if (rc.canAttackSquare(enemyPastr)) {
                 target = enemyPastr;
             }
@@ -100,10 +115,12 @@ public class NoiseTowerRobot extends BaseRobot {
         // TODO: Do spare computing for offense
     }
 
-    public boolean checkDirection(MapLocation pastr, Direction dir) {
+    public int checkDirection(MapLocation pastr, Direction dir) {
         MapLocation target = pastr;
+        int d = 0;
 
         while (rc.canAttackSquare(target)) {
+            d++;
             TerrainTile terrain = rc.senseTerrainTile(target);
             if (terrain == TerrainTile.OFF_MAP) {
                 break;
@@ -112,24 +129,27 @@ public class NoiseTowerRobot extends BaseRobot {
                 target = target.add(dir);
             }
             else {
-                MapLocation left = target.add(dir.rotateLeft().rotateLeft());
-                MapLocation right = target.add(dir.rotateRight().rotateRight());
+                MapLocation left1 = target.add(dir.rotateLeft());
+                MapLocation right1 = target.add(dir.rotateRight());
+                MapLocation left2 = target.add(dir.rotateLeft().rotateLeft());
+                MapLocation right2 = target.add(dir.rotateRight().rotateRight());
 
-                TerrainTile leftTerrain = rc.senseTerrainTile(left);
-                TerrainTile rightTerrain = rc.senseTerrainTile(right);
-
-                if (leftTerrain == TerrainTile.NORMAL || leftTerrain == TerrainTile.ROAD
-                        || rightTerrain == TerrainTile.NORMAL || rightTerrain == TerrainTile.ROAD) {
+                if ((isValidTerrain(left1) && isValidTerrain(left2))
+                        || (isValidTerrain(right1) && isValidTerrain(right2))) {
                     target = target.add(dir);
                 }
                 else {
-                    // Otherwise we are on a void (maybe off the map, need to test) and left and
-                    // right are as well
-                    return false;
+                    // Otherwise, cows will not get around
+                    return d;
                 }
             }
         }
-        return true;
+        return d;
+    }
+
+    private boolean isValidTerrain(MapLocation m) {
+        TerrainTile t = rc.senseTerrainTile(m);
+        return (t == TerrainTile.NORMAL || t == TerrainTile.ROAD);
     }
 
 }
