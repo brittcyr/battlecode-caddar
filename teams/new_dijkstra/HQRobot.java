2172098345758cd611b0767c18db07134971c559
Britt Cyr <cyrbritt@mit.edu>
2014-01-24 15:12:05 -0500

    red diff

--- teams/new_dijkstra/HQRobot.java
+++ teams/new_dijkstra/HQRobot.java
@@ -4,17 +4,11 @@ import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.GameConstants;
 import battlecode.common.RobotController;
-import battlecode.common.TerrainTile;
 
 public class HQRobot extends BaseRobot {
-    public TerrainTile[][] gameBoard;
-    public int[][]         coarseMap = null;
 
     public HQRobot(RobotController myRC) throws GameActionException {
         super(myRC);
-        int width = myRC.getMapWidth();
-        int height = myRC.getMapHeight();
-        gameBoard = new TerrainTile[width][height];
     }
 
     public void run() {
