2db5f07cbbd10957567e14dea4efec94a3a5252b
Britt Cyr <cyrbritt@mit.edu>
2014-01-23 21:43:28 -0500

    diagonal penalty testing for dijkstra

--- teams/new_dijkstra/DijkstraTest.java
+++ teams/new_dijkstra/DijkstraTest.java
@@ -18,4 +18,17 @@ public class DijkstraTest {
 
         assertTrue(java.util.Arrays.deepEquals(expected, Dijkstra.previous));
     }
+
+    @Test
+    // This test concerns the 1.4 multiplicative factor for diagonal movement
+    public void test3x3BiggerNumbers() {
+        int[][] grid = { { 10, 20, 30 }, { 10, 30, 30 }, { 10, 40, 30 } };
+
+        Dijkstra.setupDijkstra(grid, 0, 1); // grid, x, y
+        Dijkstra.doDijkstra();
+
+        int[][] expected = { { 4, 5, 6 }, { 9, 6, 6 }, { 0, 6, 7 } };
+
+        assertTrue(java.util.Arrays.deepEquals(expected, Dijkstra.previous));
+    }
 }
